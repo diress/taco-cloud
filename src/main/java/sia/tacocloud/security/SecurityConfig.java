9784ce7fc6189b40dc9c6887a84f71d3ef27afc2
Dires <diress@gmail.com>
2019-02-07 10:02:15 +0800

    security 2

--- src/main/java/sia/tacocloud/security/SecurityConfig.java
+++ src/main/java/sia/tacocloud/security/SecurityConfig.java
@@ -1,13 +1,18 @@
 package sia.tacocloud.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web
         .configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web
         .configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -16,17 +21,55 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService userDetailsService;
 
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(userDetailsService);
-        auth.inMemoryAuthentication()
-//                .withUser("buzz")
-//                .password("123")
-//                .authorities("ROLE_USER")
-//            .and()
-                .withUser("tazz")
-                .password("{noop}111")
-                .authorities("ROLE_USER");
+        auth
+            .userDetailsService(userDetailsService)
+            .passwordEncoder(encoder());
+    }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/design", "/orders")
+                .access("hasRole('ROLE_USER')")
+                .antMatchers("/", "/**").access("permitAll")
+                //end::authorizeRequests[]
+
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                //end::customLoginPage[]
+
+                // tag::enableLogout[]
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                // end::enableLogout[]
+
+                // Make H2-Console non-secured; for debug purposes
+                // tag::csrfIgnore[]
+                .and()
+                .csrf()
+                .ignoringAntMatchers("/h2-console/**")
+                // end::csrfIgnore[]
+
+                // Allow pages to be loaded in frames from the same origin; needed for H2-Console
+                // tag::frameOptionsSameOrigin[]
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+        // end::frameOptionsSameOrigin[]
+
+        //tag::authorizeRequests[]
+        //tag::customLoginPage[]
+        ;
     }
 }
