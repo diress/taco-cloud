3ce59a9fb2d790d42c5eb1c6546247dc256de170
Dires <diress@gmail.com>
2019-02-07 16:55:10 +0800

    security 3

--- src/main/java/sia/tacocloud/config/WebConfig.java
+++ src/main/java/sia/tacocloud/config/WebConfig.java
@@ -11,6 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
         @Override
         public void addViewControllers(ViewControllerRegistry registry) {
             registry.addViewController("/").setViewName("home");
+            registry.addViewController("/login");
         }
 
     }
